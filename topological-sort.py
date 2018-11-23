f6eeeacefddf58f97185021c5f9d2b60f35071ae
Naveen <gsnaveen@yahoo.com>
2018-11-23 14:28:00 -0800

    Update topological-sort.py

--- topological-sort.py
+++ topological-sort.py
@@ -1,35 +1,61 @@
 #Python program to print topological sorting of a DAG
 from collections import defaultdict
-class Graph(object):
-    def __init__(self,V):
-        self.graph = defaultdict(list)
-        self.vertices = V
-    
-    def addedge(self,v,t):
-        self.graph[v].append(t)
-    
-    def gettopologicalSortUtil(self,v,visited,stack):
-            visited[v] = True
-            for i in self.graph[v]:
-                if visited[i] == False:
-                    self.gettopologicalSortUtil(i,visited,stack)
-            stack.insert(0,v)
-            
-    def gettopologicalSort(self):
-        visited = [False]*self.vertices
-        stack = []
-        for i in range(self.vertices):
-            #print(self.graph[i])
-            if visited[i] == False:
-                self.gettopologicalSortUtil(i,visited,stack)
-        print(stack)
 
-g = Graph(6)
-g.addedge(5, 2);
-g.addedge(5, 0);
-g.addedge(2, 3);
-g.addedge(3, 1);
-g.addedge(4, 0);
-g.addedge(4, 1);
-g.vertices
-g.gettopologicalSort()   
+class solution():
+
+	def __init__(self):
+		self.graph = defaultdict(list)
+
+	def addedge(self,v,d):
+		self.graph[v].append(d)
+		if d not in self.graph.keys():
+			self.graph[d]
+
+
+	def topologicalsort(self):
+
+		visited = {}
+		stack = []
+
+		for key in self.graph.keys():
+			visited[key] = False
+
+		print(visited)
+		print(self.graph)
+
+		for key in self.graph.keys():
+
+			if visited[key] == False:
+				self.checkconected(key,visited,stack)
+
+		return stack
+
+	def checkconected(self,key,visited,stack):
+		visited[key] = True
+
+		for connected in self.graph[key]:
+			if visited[connected] == False:
+				self.checkconected(connected,visited,stack)
+
+		stack.insert(0,key)
+
+
+
+
+
+if __name__ == '__main__':
+	
+	a = solution()
+
+	a.addedge('a','c')
+	a.addedge('b','c')
+	a.addedge('b','d')
+	a.addedge('c','e')
+	a.addedge('e','h')
+	a.addedge('e','f')
+	a.addedge('d','f')
+	# a.addedge('e','h')
+	a.addedge('f','g')			
+	print(a.topologicalsort())
+
+	#['b', 'd', 'a', 'c', 'e', 'h', 'f', 'g']
