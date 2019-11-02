c82d9e25571816673423ed87e5fa265fc8b0c57f
Naveen <gsnaveen@yahoo.com>
2019-11-02 09:29:17 -0700

    Create fibonnacci_memoization.py

---
+++ fibonnacci_memoization.py
@@ -0,0 +1,26 @@
+
+from collections import defaultdict
+
+def febcached():
+
+    cache = defaultdict(int)
+
+    def feb(n):
+        valcache = cache.get(n,None)
+        if valcache:
+            # print("Cache hit",n)
+            return valcache
+        elif n < 2 :
+            val = n
+        else:
+            val = feb(n-1) + feb(n - 2)
+
+        cache[n] = val
+
+        return val
+
+    return feb
+
+myFeb = febcached()
+
+print(myFeb(100))
