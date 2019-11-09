c28d8ebbc912472881fc85726a9ef751ad127944
Naveen <gsnaveen@yahoo.com>
2019-11-09 13:41:37 -0800

    Create findPeakElement.py

---
+++ findPeakElement.py
@@ -0,0 +1,22 @@
+class solution:
+
+    def findPeakElement(self,nums) -> int:
+        index = 1;value = 0
+        la = len(nums)
+        for i in range(1,la -1):
+            if  (nums[i] > nums[i -1]) and (nums[i] > nums[i + 1]):
+                value = max(value,nums[i])
+                if value == nums[i]:
+                    index = i
+
+        return (index,value)
+
+
+if __name__ == '__main__':
+    a = solution()
+
+    nums = [1,2,3,1]
+    print(a.findPeakElement(nums))
+
+    nums = [1, 2, 1, 3, 5, 6, 4]
+    print(a.findPeakElement(nums))
