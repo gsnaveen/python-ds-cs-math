c138e32d2edd4e3f5b30c6c4993debdb44610792
Naveen <gsnaveen@yahoo.com>
2019-11-09 12:15:27 -0800

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -14,10 +14,33 @@ def binarySearch(arr,l,r,x):
     else:
         return -1
 
+# Searching the rotated sorted list
+def binarySearchR(arr,l,r,x):
 
-x = 10
+    pivot = findPivot(arr)
+
+    if arr[pivot + 1] <= x and arr[r] >= x:
+        l = pivot + 1
+        returnVal = binarySearch(arr, l, r, x)
+        return returnVal
+    elif arr[l] <= x and arr[pivot] >= x:
+        r = pivot
+        return binarySearch(arr, l, r, x)
+
+# Finding a Pivot
+def findPivot(arr):
+    for i in range(1,len(arr)):
+        if arr[i -1 ] > arr[i]:
+            return i -1
 
 # Function call
+arr = [4,5,6,7,0,1,2]; x= target =0
+print(binarySearchR(arr, 0, len(arr ) -1, x))
+
+
+
+# Function call
+x=10
 arr = [ 2, 3, 4, 7, 10, 40 ]
 assert binarySearch(arr, 0, len(arr ) -1, x) == 4
 arr = [ 2, 3, 4, 7, 10]
